b238371c4847590e9b00f2f4661de488f45d179f
Abhi <programwithabhi@gmail.com>
2022-02-28 14:45:08 +0530

    Final Commit

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,15 +8,15 @@ export class NewsItem extends Component {
         let {url} = this.props;
         return (
             <div>
-                <div className="card my-4 mx-4" style={{width: "18rem"}}>
+                <ddddiv className="card my-4 mx-4" style={{width: "18rem"}}>
                     <img src={url} className="card-img-top" alt="..." />
                     <div className="card-body text-center">
                         <h5 className="card-title">Card title</h5>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         <a href="/" className="btn  btn-sm btn-primary">Read More</a>
                     </div>
-                </div>
-            </div>
+                </ddddiv>
+        </div>
         )
     }
 }
