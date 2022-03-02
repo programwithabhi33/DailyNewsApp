e1b61d9b5ab218157a695c873fe16a21cb1c68a0
Abhi <programwithabhi@gmail.com>
2022-03-02 17:04:03 +0530

    Updating the news.js and also refining the code usebility

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@
 
 // function App() {
 //   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
+//     <div abhiName="App">
+//       <header abhiName="App-header">
+//         <img src={logo} abhiName="App-logo" alt="logo" />
 //         <p>
 //           Edit <code>src/App.js</code> and save to reload.
 //         </p>
 //         <a
-//           className="App-link"
+//           abhiName="App-link"
 //           href="https://reactjs.org"
 //           target="_blank"
 //           rel="noopener noreferrer"
@@ -27,12 +27,33 @@ import React, { Component } from 'react'
 import Navbar from './components/Navbar'
 import News from './components/News'
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 export class App extends Component {
   render() {
+    let api_key="a66d2f246de343bdaa7fc198e799cb2c";
+    let pageSize=12;
     return (
       <div>
+        <Router>
         <Navbar/>
-        <News/>
+          <Routes>
+            {/* exact means the url path match to the exact path */}
+            {/* /user --> when the endpoint is not defined on your path and exact is not set then react will match the /user/id or elsewhere start with /user/something */}
+            <Route exact path='/' element={<News key="general" pageSize={pageSize} api_key={api_key} country='in' category="general" />} />
+            <Route exact path='/business' element={<News key="business" pageSize={pageSize} api_key={api_key} country='in' category="business"/>} />
+            <Route exact path='/science' element={<News key="science" pageSize={pageSize} api_key={api_key} country='in' category="science"/>} />
+            <Route exact path='/sports' element={<News key="sports" pageSize={pageSize} api_key={api_key} country='in' category="sports"/>} />
+            <Route exact path='/entertainment' element={<News key="entertainment" pageSize={pageSize} api_key={api_key} country='in' category="entertainment"/>} />
+             <Route exact path='/technology' element={<News key="technology" pageSize={pageSize} api_key={api_key} country='in' category="technology"/>} />
+            <Route exact path='/health' element={<News key="health" pageSize={pageSize} api_key={api_key} country='in' category="health"/>} />
+          </Routes> 
+        </Router>
+        
       </div>
     )
   }
--- src/components/News.js
+++ src/components/News.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner'
+import PropTypes from 'prop-types'
+
 
 export class News extends Component {
+     
+    // Setting up the default propstypes if the props doesn't pass
+    static defaultProps={
+        country:'in',
+        page:1,
+        category:'general',
+    }
+    // Setting up the proptypes datatypes of their corresponding variable
+    static propTypes={
+        country:PropTypes.string.isRequired,
+        page:PropTypes.number.isRequired,
+        category:PropTypes.string.isRequired,
+    }
 
     // Creating class constructor
     constructor() {
@@ -10,110 +26,96 @@ export class News extends Component {
         // console.log("Im the news.js constructor")
 
         // Defining the states and you can update state using this.setState 
-        this.state={
+        this.state = {
             articles: [],
             loading: false,
-            page:1,
+            page: 0,
         }
+        console.log(this.state.articles)
     }
 
-    handlePrevClick = async()=>{
-        // console.log("Clicked on prev button")
-        // Updating the state variables,Note:-the setState is a callback function 
-        this.setState({
-            page:this.state.page - 1,
-        },()=>{
-            // console.log("The setState of previousbutton has been fired")
-        })
-        // console.log(this.state.page)
-        
-        // Defining the page variable
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey={your_api_key}&page=${this.state.page-1}`
-        // console.log(url)
-        
-        // Fetching the news from page variable
-        let data = await fetch(url)
-        
-        // Convert the raw data to json using .json function
-        let parsedData = await data.json()
-        
-        // Set the this.state.articles to the newest articles of corresponding page
-        this.setState({articles:parsedData.articles})
 
-    }
+    async componentDidMount() {
+        this.updateNews()
 
-    handleNextClick = async()=>{
-        // This is condition for the pagination,it can handle the last page and do not fecth the non-existing pages when user clicking the next button by default we aslo  handled the next button disabled when the last page occur
-        if(this.state.page +1 > Math.ceil(this.state.totalResults/20)){
-        //  console.log("If condition fired")
-        }
-
-        else{
-        //  console.log("Else condition fired")   
-        // console.log("Clicked on Next button")
-        
-        // Updating the state variables,Note:-the setState is a callback function 
-        this.setState({
-            page:this.state.page + 1,
-        },()=>{
-            // console.log("The setState of next function is fired")
-        })
-        // console.log(this.state.page)
-        // Defining the page variable
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey={your_api_key}&page=${this.state.page+1}`
-        // console.log(url)
-        
-        // Fetching the newest news from corresponding page
-        let data = await fetch(url)
-
-        // Convert the raw data to json using .json function
-        let parsedData = await data.json()
-         
-        // Set the this.state.articles to the newest articles of corresponding page
-        this.setState({articles:parsedData.articles})
-    }
-
-    }
-
-    async componentDidMount(){
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey={your_api_key}&page=${this.state.page}`
-        // console.log(url)
-        
-        // Fetching the url using fetch api
-        let data = await fetch(url)
-
-        // Parsing the incoming data using .json() function,fetch api return raw data so you can parse the data using .json function
-        let parsedData = await data.json()
-        // console.log(parsedData)
-
-        // Updating the this.articles class variable using setState function 
-        this.setState({articles: parsedData.articles,totalResults:parsedData.totalResults})
-        // console.log(this.articles)
-        // console.log(this.articles)
-        
     }
     
+    async updateNews(){
+         // This is condition for the pagination,it can handle the last page and do not fecth the non-existing pages when user clicking the next button by default we aslo  handled the next button disabled when the last page occur
+         if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
+            console.log("If condition fired")
+            // console.log(this.state.page)
+            // Defining the page variable
+            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.api_key}&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`
+            // console.log(url)
+
+            this.setState({ loading: true })
+            // Fetching the newest news from corresponding page
+            let data = await fetch(url)
+
+            // Convert the raw data to json using .json function
+            let parsedData = await data.json()
+
+            // Set the this.state.articles to the newest articles of corresponding page
+            this.setState({
+                articles: parsedData.articles,
+                totalResults: parsedData.totalResults,
+                loading: false
+            })
+            console.log("this is the total results "+this.state.totalResults)
+        }
+       
+        
+    }
+    handlePrevClick = async () => {
+        this.updateNews()
+
+        // Updating the state variables,Note:-the setState is a callback function 
+        this.setState({
+            page: this.state.page -1,
+        }, () => {
+            console.log("The setState of next function is fired")
+        })
+        
+
+    }
+
+    handleNextClick = async () => {
+        this.updateNews()
+       
+        // Updating the state variables,Note:-the setState is a callback function 
+        this.setState({
+            page: this.state.page + 1,
+        }, () => {
+            console.log("The setState of next function is fired")
+        })
+       
+    }
+
 
 
     render() {
         return (
-            <div className="container my-4  px-4 py-4">
-                <h2>Welcome To The MyNewsApp - Grab the latest news here :</h2>
-                <div className="row">
-                    {/* map is a higher order array method to iterate each element of an array and return the corresponding div tag of each element */}
-                    {/* You should pass the unique key to the each element of the array  */}
-                    {this.state.articles.map((element)=> {
-                        return <div className="col-md-6 col-lg-4" key={element.url}>
-                            <NewsItem title={element.title} desc={element.description} imageUrl={element.urlToImage} url={element.url}/>
-                        </div>
-                    })}
+            <>
+                <div className="container my-4  px-4 py-4">
+                    <h2 className="text-center">Welcome To The MyNewsApp - Grab the latest news here :</h2>
+                    {this.state.loading && <Spinner />}
+                    <div className="row">
+                        {/* map is a higher order array method to iterate each element of an array and return the corresponding div tag of each element */}
+                        {/* You should pass the unique key to the each element of the array  */}
+                        {!this.state.loading && this.state.articles.map((element) => {
+                            return <div className="col-md-6 col-lg-4" key={element.url}>
+                                <NewsItem title={element.title} desc={element.description} imageUrl={element.urlToImage} url={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+                            </div>
+                        })}
 
+                    </div>
+                    <div className="container d-flex justify-content-between">
+                        <button disabled={this.state.page <= 1} className="btn btn-dark" onClick={this.handlePrevClick}>&larr;Prev</button>
+                        <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} className="btn btn-dark" onClick={this.handleNextClick}>Next&rarr;</button>
+                    </div>
                 </div>
-                <div className="container d-flex justify-content-between">
-                    <button disabled={this.state.page<=1} className="btn btn-dark" onClick={this.handlePrevClick}>&larr;Prev</button>
-                    <button disabled={this.state.page>1} className="btn btn-dark" onClick={this.handleNextClick}>Next&rarr;</button>
-                </div>
-            </div>
+            </>
         )
     }
 }
