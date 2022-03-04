33ae8fb692bdd04dd5fc66717689f8b2170657fe
Abhi <programwithabhi@gmail.com>
2022-03-04 16:57:26 +0530

    Adding top loading bar and adding the infinite scroll

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@
 import React, { Component } from 'react'
 import Navbar from './components/Navbar'
 import News from './components/News'
+import LoadingBar from 'react-top-loading-bar'
 
 import {
   BrowserRouter as Router,
@@ -34,25 +35,47 @@ import {
 } from "react-router-dom";
 
 export class App extends Component {
+  state={
+    progress:0
+  }
+  setAbhi(){
+    console.log("im the setAbhi function ")
+    console.log(this.setState)
+  }
+   setProgress=(progress)=>{
+    console.log("im the setProgress function ")
+    console.log(this.setState)
+    this.setState({
+      progress:progress
+    })
+    this.setAbhi()
+  }
+
+ 
   render() {
-    let api_key="a2b0210ee5604b0f8b6bd2e6f33c80a3";
-    let pageSize=12;
+    let api_key=process.env.REACT_APP_API_KEY;
+    let pageSize=6;
     return (
       <div>
         <Router>
+        <LoadingBar
+        height={3}
+        color='#09ed1c'
+        progress={this.state.progress}
+        />
         <Navbar/>
           <Routes>
             {/* exact means the url path match to the exact path */}
             {/* /user --> when the endpoint is not defined on your path and exact is not set then react will match the /user/id or elsewhere start with /user/something */}
 
-            {/* When you render a same element in diffierent Route react is not updating the component because react by default knows that this component is already in the dom so why i readd the component,to bypass the behaviour you must pass the unique key to component that means react load the same component because react sees that key is diffierent than previous */}
-            <Route exact path='/' element={<News key="general" pageSize={pageSize} api_key={api_key} country='in' category="general" />} />
-            <Route exact path='/business' element={<News key="business" pageSize={pageSize} api_key={api_key} country='in' category="business"/>} />
-            <Route exact path='/science' element={<News key="science" pageSize={pageSize} api_key={api_key} country='in' category="science"/>} />
-            <Route exact path='/sports' element={<News key="sports" pageSize={pageSize} api_key={api_key} country='in' category="sports"/>} />
-            <Route exact path='/entertainment' element={<News key="entertainment" pageSize={pageSize} api_key={api_key} country='in' category="entertainment"/>} />
-             <Route exact path='/technology' element={<News key="technology" pageSize={pageSize} api_key={api_key} country='in' category="technology"/>} />
-            <Route exact path='/health' element={<News key="health" pageSize={pageSize} api_key={api_key} country='in' category="health"/>} />
+            {/* When you render a same element in diffierent Route react is not updating the component because react by default knows that this component is already in the dom so why i readd the component,to bypass the behaviour you must pass the unique key to component that means react load the corresponding component because react sees that key is diffierent than previous component  */}
+            <Route exact path='/' element={<News setProgress={this.setProgress}  key="general" pageSize={pageSize} api_key={api_key} country='in' category="General" />} />
+            <Route exact path='/business' element={<News setProgress={this.setProgress}  key="business" pageSize={pageSize} api_key={api_key} country='in' category="Business"/>} />
+            <Route exact path='/science' element={<News setProgress={this.setProgress}  key="science" pageSize={pageSize} api_key={api_key} country='in' category="Science"/>} />
+            <Route exact path='/sports' element={<News setProgress={this.setProgress}  key="sports" pageSize={pageSize} api_key={api_key} country='in' category="Sports"/>} />
+            <Route exact path='/entertainment' element={<News setProgress={this.setProgress}  key="entertainment" pageSize={pageSize} api_key={api_key} country='in' category="Entertainment"/>} />
+             <Route exact path='/technology' element={<News setProgress={this.setProgress}  key="technology" pageSize={pageSize} api_key={api_key} country='in' category="Technology"/>} />
+            <Route exact path='/health' element={<News setProgress={this.setProgress}  key="health" pageSize={pageSize} api_key={api_key} country='in' category="Health"/>} />
           </Routes> 
         </Router>
         
--- src/components/News.js
+++ src/components/News.js
@@ -2,21 +2,22 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner'
 import PropTypes from 'prop-types'
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 
 export class News extends Component {
-     
+
     // Setting up the default propstypes if the props doesn't pass
-    static defaultProps={
-        country:'in',
-        page:0,
-        category:'general',
+    static defaultProps = {
+        country: 'in',
+        page: 0,
+        category: 'general',
     }
     // Setting up the proptypes datatypes of their corresponding variable
-    static propTypes={
-        country:PropTypes.string.isRequired,
-        page:PropTypes.number.isRequired,
-        category:PropTypes.string.isRequired,
+    static propTypes = {
+        country: PropTypes.string.isRequired,
+        page: PropTypes.number.isRequired,
+        category: PropTypes.string.isRequired,
     }
 
     // Creating class constructor
@@ -29,114 +30,155 @@ export class News extends Component {
         this.state = {
             articles: [],
             loading: false,
-            page: 0,
+            page: 1,
+            totalResults: 0
         }
-        // console.log(this.state.articles)
+
     }
+
 
 
     async componentDidMount() {
         // Calling the updateNews function 
+        // console.log("component didMount fired")
         this.updateNews()
 
     }
-    
-    async updateNews(){
-         // This is condition for the pagination,it can handle the last page and do not fecth the non-existing pages when user clicking the next button by default we aslo  handled the next button disabled when the last page occur
-         if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
-            // console.log("If condition fired")
-            // console.log(this.state.page)
-            // Defining the page variable
-            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.api_key}&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`
-            // console.log(url)
-            this.setState({ loading: true })
-            // console.log("the loading is true now")
-            // Fetching the newest news from corresponding page
-            let data = await fetch(url)
 
-            // Convert the raw data to json using .json function
-            let parsedData = await data.json()
-
-            // Set the this.state.articles to the newest articles of corresponding page
-            this.setState({
-                articles: parsedData.articles,
-                totalResults: parsedData.totalResults,
-                loading: false,
-
-                // Here the page value in constructor is 0 and when the api fetch we pass the page +1 because it configure detct page variable as 0 and setState will is a asynchronous function   
-                page:this.state.page + 1  
+    async updateNews() {
+        this.props.setProgress(10)
+        // console.log("updateNews has been fired")
+        // This is condition for the pagination,it can handle the last page and do not fecth the non-existing pages when user clicking the next button by default we aslo  handled the next button disabled when the last page occur
+        // if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
+        // console.log("If condition fired")
+        // Defining the page variable
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.api_key}&page=${this.state.page}&pageSize=${this.props.pageSize}`
+        this.setState({ loading: true }
+            , () => {
             })
-            // console.log("the loading is false now")
-           
-        }
-       
-        
+        // Fetching the newest news from corresponding page
+        let data = await fetch(url)
+        this.props.setProgress(40)
+        // Convert the raw data to json using .json function
+        let parsedData = await data.json()
+        this.props.setProgress(70)
+        // Set the this.state.articles to the newest articles of corresponding page
+        this.setState({
+            articles: parsedData.articles,
+            totalResults: parsedData.totalResults,
+            loading: false,
+
+            // Here the page value in constructor is 0 and when the api fetch we pass the page +1 because it configure detct page variable as 0 and setState will is a asynchronous function    
+        }, () => {
+        })
+        this.props.setProgress(100)
+
+
+        // }
+
+
     }
     // handlePrevClick = async () => {
-    //     this.updateNews()
-
+    //     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.api_key}&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`
     //     // Updating the state variables,Note:-the setState is a callback function 
     //     this.setState({
-    //         page: this.state.page -1,
+    //         page: this.state.page - 1,
     //     }, () => {
-    //         console.log("The setState of next function is fired")
+    //         // console.log("The setState of render of method in previous button "+ this.state.page)
+    //         this.updateNews(url)
     //     })
-        
+
+
 
     // }
 
-    // handleNextClick = async() => {
-    //     this.updateNews()
-       
+    // handleNextClick = async () => {
+    //     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.api_key}&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`
     //     // Updating the state variables,Note:-the setState is a callback function 
     //     this.setState({
     //         page: this.state.page + 1,
     //     }, () => {
-    //         console.log("The setState of next function is fired")
+
+    //         // console.log(" In callback : The setState of render of method in next button "+ this.state.page)
+    //         this.updateNews(url)
     //     })
-       
+
+    //     // console.log("The setState of render of method in next button "+ this.state.page)
+
     // }
+
+    fetchMoreData = async () => {
+        this.setState({
+            page: this.state.page + 1,
+        }, () => {
+            console.log("this is a callback function " + this.state.page)
+        })
+        console.log(this.state.page)
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.api_key}&page=${this.state.page}&pageSize=${this.props.pageSize}`
+
+        let data = await fetch(url)
+
+        let parsedData = await data.json()
+
+        this.setState({
+            articles: this.state.articles.concat(parsedData.articles),
+            totalResults: parsedData.totalResults,
+        })
+    }
 
 
 
     render() {
+        // Here do not update the state variables 
+        // Update state variables in returen
+        // console.log("Render function fired")
         return (
             <>
                 <div className="container my-4  px-4 py-4">
-                    <h2 className="text-center">Welcome To The MyNewsApp - Grab the latest news here :</h2>
+                    <h2 className="text-center">Top Headlines From: {this.props.category}</h2>
                     {this.state.loading && <Spinner />}
-                    <div className="row">
-                        {/* map is a higher order array method to iterate each element of an array and return the corresponding div tag of each element */}
-                        {/* You should pass the unique key to the each element of the array  */}
-                        {!this.state.loading && this.state.articles.map((element) => {
-                            return <div className="col-md-6 col-lg-4" key={element.url}>
-                                <NewsItem title={element.title} desc={element.description} imageUrl={element.urlToImage} url={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
-                            </div>
-                        })}
+                    <InfiniteScroll
+                        dataLength={this.state.articles.length}
+                        next={this.fetchMoreData}
+                        hasMore={this.state.articles.length !== this.state.totalResults}
+                        loader={<h4><Spinner /></h4>}
+                    >
 
-                    </div>
-                    <div className="container d-flex justify-content-between">
-                        <button disabled={this.state.page <= 1} className="btn btn-dark" onClick={()=>{
-                            this.updateNews()
+                        <div className="row">
+                            {/* map is a higher order array method to iterate each element of an array and return the corresponding div tag of each element */}
+                            {/* You should pass the unique key to the each element of the array  */}
+                            {this.state.articles.map((element) => {
+                                return <div className="col-md-6 col-lg-4" key={element.url}>
+                                    <NewsItem title={element.title} desc={element.description} imageUrl={element.urlToImage} url={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+                                </div>
+                            })}
+                            {/* {console.log("The infiniteScroll has been fired "+this.state.articles)} */}
+                        </div>
+
+                    </InfiniteScroll>
+
+                    {/* <div className="container d-flex justify-content-between">
+                        <button disabled={this.state.page <= 1} className="btn btn-dark" onClick={() => {
+                            // Updating the state variables,Note:-the setState is a callback function 
                             this.setState({
-                                // Updating the state variables,Note:-the setState is a callback function 
-                                page: this.state.page -1,
+                                page: this.state.page - 1,
                             }, () => {
-                                // console.log("The setState of next function is fired")
+
+                                // console.log(" In callback : The setState of render of method in next button "+ this.state.page)
+                                this.updateNews()
                             })
-                            
                         }}>&larr;Prev</button>
-                        <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} className="btn btn-dark" onClick={()=>{
-                            this.updateNews()
+                        <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} className="btn btn-dark" onClick={() => {
                             // Updating the state variables,Note:-the setState is a callback function 
                             this.setState({
                                 page: this.state.page + 1,
                             }, () => {
-                                // console.log("The setState of next function is fired")
+
+                                // console.log(" In callback : The setState of render of method in next button "+ this.state.page)
+                                this.updateNews()
                             })
-                            
                         }}>Next&rarr;</button>
-                    </div>
+                    </div> */}
                 </div>
             </>
         )
